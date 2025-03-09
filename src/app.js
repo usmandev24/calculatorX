b06cefb06a67be14fa57d44a2d87dca2e9433cb3
Usman Ghani <usman.dev24@gmail.com>
2025-03-09 21:20:55 +0500

    basic evaluation comleted

--- src/app.js
+++ src/app.js
@@ -50,16 +50,18 @@ class Interface {
 class State {
   constructor(ui) {
     this.ui = ui;
-    this.input = document.querySelector("input");
+    this.input = document.querySelector("textarea");
     this.instCalculation = document.getElementById("preData");
     this.btns = Array.from(document.querySelectorAll(".st-btn"));
+    this.hist = document.getElementById("histItems");
     this.dataValid = "123456789+-*×÷/0.()^%√";
     this.vldTouchBtns = "1234567890+-×÷.%^√";
-    this.oper = "+-*×÷/^%.";
+    this.oper = "+-*×÷/^.";
     this.preValue = "";
     this.preSelection = 1;
   }
   start() {
+    let result;
     this.touchBtnStart(this.btns);
     this.input.addEventListener("keypress", (event) => {
       this.preSelection = this.input.selectionEnd;
@@ -71,16 +73,20 @@ class State {
         this.input.value = this.preValue;
         this.input.selectionEnd = this.preSelection;
       }
+      if (event.key == "Enter") {
+        this.updateHist(result);
+        this.preValue = result
+        this.input.value = result
+      }
     });
     this.input.addEventListener("input", (event) => {
       this.preSelection = this.input.selectionEnd;
       [this.preValue, this.preSelection] = this.validater(
-        this.input,
         this.preValue,
         this.preSelection,
-        this.dataValid,
-        this.oper,
       );
+      result = this.praser()
+      this.updateInstCal(result);
     });
   }
 
@@ -89,58 +95,69 @@ class State {
       btn.addEventListener("click", () => {
         this.input.focus();
         this.preSelection = this.input.selectionEnd;
-        [this.preValue , this.preSelection] = this.touchInputValidater(this.preValue, this.preSelection, btn.textContent)
+        [this.preValue, this.preSelection] = this.touchInputValidater(
+          this.preValue,
+          this.preSelection,
+          btn.textContent,
+        );
+        this.updateInstCal(this.praser())
       });
     }
   }
   touchInputValidater(pvalue, pselection, btnContent) {
-    console.log(this.input.selectionEnd, pselection);
     let value = this.input.value;
+    let curntSelection = pselection;
     if (this.vldTouchBtns.includes(btnContent)) {
       value =
         value.slice(0, pselection) +
         btnContent +
         value.slice(pselection, value.length);
-        pselection+=1;
+      curntSelection += 1;
     } else if (btnContent == "( )") {
       value =
         value.slice(0, pselection) +
         "()" +
         value.slice(pselection, value.length);
-        pselection+=1;
-    } else if (btnContent == "←" && pselection !=0) {
+      curntSelection += 1;
+    } else if (btnContent == "←" && pselection != 0) {
       value =
-        value.slice(0, pselection-1) +
-        value.slice(pselection, value.length);
-     pselection -=1;
+        value.slice(0, pselection - 1) + value.slice(pselection, value.length);
+      curntSelection -= 1;
     } else if (btnContent == "AC") {
       value = "";
-      pselection = 0;
+      curntSelection = 0;
     } else if (btnContent == "PI") {
       value =
-        value.slice(0, pselection) + "3.14" +
+        value.slice(0, pselection) +
+        "3.14" +
         value.slice(pselection, value.length);
-      pselection += 4;
+      curntSelection += 4;
     }
-    pvalue = value;
     this.input.value = value;
-    this.input.selectionEnd = pselection;console.log(this.input.selectionEnd, pselection);
-    return [pvalue, pselection ]
+    this.input.selectionEnd = curntSelection;
+    return this.opChecker(pvalue, curntSelection);
   }
-  // pvalue = previous vallid value of input,
-  //input = dom element, pvlaue= string, pselection = number, datavalid = string, oper = string
-  validater(input, pvalue, pselection, dataValid, oper) {
+
+  validater(pvalue, pselection) {
+    let input = this.input;
     if (
-      !dataValid.includes(input.value[input.selectionEnd - 1]) &&
+      !this.dataValid.includes(input.value[input.selectionEnd - 1]) &&
       input.value.length !== 0 &&
       input.selectionEnd != 0
     ) {
       input.value = pvalue;
       input.selectionEnd = pselection - 1;
     }
+    input.value = input.value.replace("*", "×");
+    input.value = input.value.replace("/", "÷");
+    return this.opChecker(pvalue, pselection);
+  }
+
+  opChecker(pvalue, pselection) {
+    let input = this.input;
     if (
-      oper.includes(input.value[input.selectionEnd - 2]) &&
-      oper.includes(input.value[input.selectionEnd - 1])
+      this.oper.includes(input.value[input.selectionEnd - 2]) &&
+      this.oper.includes(input.value[input.selectionEnd - 1])
     ) {
       input.value =
         pvalue.slice(0, input.selectionEnd - 2) +
@@ -149,18 +166,48 @@ class State {
       input.selectionEnd = pselection - 1;
     }
     if (
-      oper.includes(input.value[input.selectionEnd - 3]) &&
+      this.oper.includes(input.value[input.selectionEnd - 3]) &&
       "()".includes(input.value[input.selectionEnd - 2]) &&
-      "*/^%.".includes(input.value[input.selectionEnd - 1])
+      "*/^.×÷".includes(input.value[input.selectionEnd - 1])
     ) {
       input.value = pvalue;
       input.selectionEnd = pselection - 1;
     }
+    this.input.value = input.value;
     pvalue = input.value;
     return [pvalue, pselection];
   }
-  praser() {}
-  evaluator() {}
+
+  praser() {
+    let value = this.input.value;
+    value = value.replaceAll("×", "*")
+    .replaceAll("÷", "/")
+    .replaceAll("^", "**")
+    .replaceAll("%", "/100");
+    console.log(value);
+    try {
+      let instResult = Number(eval(value));
+      if (value.length == 0) {
+        return "0";
+      }
+      if (instResult/Number(instResult.toFixed(0)) === 1) {
+        return instResult;
+      }
+      return instResult.toFixed(3);
+    } catch {
+      return "?";
+    }
+  }
+  updateInstCal(result) {
+    this.instCalculation.textContent = "";
+    this.instCalculation.textContent = ` = ${result}`;
+  }
+  updateHist(result) {
+    let item = document.createElement("p");
+    let value = document.createTextNode(`${this.preValue} = ${result} `);
+    item.appendChild(value)
+    this.hist.appendChild(item);
+  }
 }
 let interface = new Interface();
 interface.setEvents();
